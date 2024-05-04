c3ce0d82a392f6b42b75e204e9b438e3590c849f
rencewang <47406212+rencewang@users.noreply.github.com>
2024-05-03 22:26:36 -0700

    org files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="overall">
+      {/* take in a header component */}
+      <header className="header">
+        <h1>My React App</h1>
       </header>
-    </div>
+
+      {/* this is the rendering space to hold popups */}
+      <section></section>
+
+      {/* take in the map component */}
+      <section></section>
+
+      {/* take in a footer component */}
+      <footer></footer>
+    </main>
   );
 }
 
