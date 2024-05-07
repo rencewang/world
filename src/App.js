3518595d8ec9df36e8c72dac28baf35a819fffcc
rencewang <47406212+rencewang@users.noreply.github.com>
2024-05-07 14:09:00 +0300

    use header

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import Header from './components/header';
 function App() {
   return (
     <main className="overall">
-      {/* take in a header component */}
-      <header className="header">
-        <h1>My React App</h1>
-      </header>
+      <Header />
 
       {/* this is the rendering space to hold popups */}
       <section></section>
 
-      {/* take in the map component */}
       <Map />
 
       {/* take in a footer component */}
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 function Header() {
-  return <header></header>;
+  return <header>map</header>;
 }
 
 export default Header;
