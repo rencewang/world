2c7d6ba1f6ac10e3b55ee577d2243c56da8f3d1e
rencewang <47406212+rencewang@users.noreply.github.com>
2024-05-17 21:55:44 -0700

    add popup

---
+++ src/components/popup.js
@@ -0,0 +1,12 @@
+// component for displaying details of location or flight segment
+
+function Popup() {
+  return (
+    <div>
+      <div>location details</div>
+      <div>flight segment details</div>
+    </div>
+  );
+}
+
+export default Popup;
